4965851c59360f6c83e81e4f4e6e393c7c3725c1
Raissa Santos22 <raissa.santos171@etec.sp.gov.br>
2025-11-18 18:58:23 -0300

    O primeiro passo do projeto é criar as classes modelo : Produto e Cliente, colocar os seus respectivos atributos e os métodos getters e setters

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,60 @@
-public class Cliente {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Cliente {
+    private int id;
+    private String nome;
+    private String email;
+    private String genero;
+    private boolean receberNotificacao;
+    private boolean receberEmail;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getGenero(){
+        return genero;
+    }
+
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+
+    public boolean isReceberEmail() {
+        return receberEmail;
+    }
+
+    public void setReceberEmail(boolean receberEmail) {
+        this.receberEmail = receberEmail;
+    }
+
+    public boolean isReceberNotificacao() {
+        return receberNotificacao;
+    }
+
+    public void setReceberNotificacao(boolean receberNotificacao) {
+        this.receberNotificacao = receberNotificacao;
+    }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,47 @@
 public class Produto {
+    private int id;
+    private String nome;
+    private int quantidade;
+    private double PrecoUnitario;
+    private String fornecedor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return PrecoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        PrecoUnitario = precoUnitario;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
 }
