e5e60aa17eb6ab3c740612611235037b25d7b608
Raissa-Santos22 <raissa.santos171@etec.sp.gov.br>
2025-11-28 15:01:31 -0300

    fazendo testes

--- src/Conexao.java
+++ src/Conexao.java
@@ -1,3 +1,17 @@
-public class Conexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Conexao {
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/cadastro2";
+            String user = "root"; // troque pelo seu usuário
+            String password = ""; // troque pela sua senha
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na conexão: " + e.getMessage());
+        }
+    }
 }
+
